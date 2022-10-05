5bfef59b56b0b99eecfb8b6c062083a424a8cb45
Mo Ali <akhter.ali@upstart.com>
2022-10-05 15:40:18 -0400

    - redo key mapping

--- nvim/init.lua
+++ nvim/init.lua
@@ -109,8 +109,8 @@ end)
 
 -- telescope
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>o', builtin.find_files, {})
+vim.keymap.set('n', '<leader>p', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
 vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
 
