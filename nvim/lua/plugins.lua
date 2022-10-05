192b36c130ff703fe78c8ab6efdc945ade1410cf
Mo Ali <akali1987@gmail.com>
2022-10-05 16:12:59 -0400

    Remove commented code

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -68,23 +68,6 @@ return packer.startup(function()
     end
   })
 
-  -- snap
-  --[[ use ({
-    "camspiers/snap",
-    requires = "nvim-treesitter",
-    rocks = { "fzy" }, -- This does not work everywhere, need to find fixes
-    config = function ()
-      local snap = require("snap")
-      local consumer = snap.get"consumer.fzy"
-      local file = snap.config.file:with { preview = true, consumer = consumer }
-      local grep = snap.config.vimgrep:with { consumer = consumer }
-      snap.maps {
-        {"<C-o>", file { producer = "fd.file", suffix = " Files❯" }},
-        {"<C-p>", grep { consumer = "fzy",  producer = "ripgrep.vimgrep", suffix = " Grep❯" }},
-      }
-    end
-  }) ]]
-
   use ({
     "nvim-telescope/telescope.nvim", tag = "0.1.0",
   -- or                            , branch = '0.1.x',
@@ -133,32 +116,6 @@ return packer.startup(function()
       require("telescope").load_extension("fzy_native")
     end
   })
-
-  --[[ use ({
-    "nvim-telescope/telescope-fzf-native.nvim",
-    run = "make",
-    requires = {
-      "nvim-telescope/telescope.nvim"
-    },
-    config = function ()
-      -- You dont need to set any of these options. These are the default ones. Only
-      -- the loading is important
-      require("telescope").setup {
-        extensions = {
-          fzf = {
-            fuzzy = true,                    -- false will only do exact matching
-            override_generic_sorter = true,  -- override the generic sorter
-            override_file_sorter = true,     -- override the file sorter
-            case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                             -- the default case_mode is "smart_case"
-          }
-        }
-      }
-      -- To get fzf loaded and working with telescope, you need to call
-      -- load_extension, somewhere after setup function:
-      require("telescope").load_extension("fzf")
-    end
-  }) ]]
 
   -- lspinstaller
   use({
