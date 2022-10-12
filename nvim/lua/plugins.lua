2e21277b7c23d023d19379218b10539603f77265
Mo Ali <akali1987@gmail.com>
2022-10-12 10:44:43 -0400

    Remove commented code

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -106,11 +106,6 @@ return packer.startup(function()
   use ({
   "folke/which-key.nvim",
     config = function()
-      --[[ require("which-key").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      } ]]
       require("config.dap.keymaps").setup()
     end
   })
