a846c0e28bb39bcbeeab0a3d9da54635d0bfa5d8
Mo Ali <akali1987@gmail.com>
2022-10-14 08:06:25 -0400

    Remove call for dap lua

--- nvim/lua/dap/init.lua
+++ nvim/lua/dap/init.lua
@@ -51,7 +51,7 @@ local function configure_exts()
 end
 
 local function configure_debuggers()
-  require("config.dap.lua").setup()
+  -- require("config.dap.lua").setup()
   -- require("config.dap.python").setup()
   -- require("config.dap.rust").setup()
   require("config.dap.go").setup()
