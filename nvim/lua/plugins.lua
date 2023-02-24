e2852dd75ac2ecbaa70f1d96198e6d37d57948e9
Mo Ali <akhter.ali@upstart.com>
2023-02-24 17:04:43 -0500

    Add troble

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -109,6 +109,19 @@ return packer.startup(function()
         }
     })
 
+    -- Trouble
+    use({
+        "folke/trouble.nvim",
+        requires = "nvim-tree/nvim-web-devicons",
+        config = function()
+            require("trouble").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    })
+
     use ({
         'Shatur/neovim-ayu',
         config = function ()
