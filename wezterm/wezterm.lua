c8ba31c9c58ff7b9128bea1f2212a50cb6e0eb45
Mo Ali <akali1987@gmail.com>
2023-04-29 12:37:10 -0400

    Fix conflict

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require 'wezterm'
 
 local config = {}
 config.color_scheme = 'ayu'
+-- config.color_scheme = 'Material (Gogh)'
 config.font = wezterm.font('Fira Code')
 config.term = 'wezterm'
 config.keys = {
