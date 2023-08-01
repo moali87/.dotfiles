8e816f270b275c468f1cfc8b879330134c53486e
Mo Ali <akali1987@gmail.com>
2023-07-31 23:02:11 -0400

    Add trouble

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -14,6 +14,15 @@ local plugins = {
             "rcarriga/nvim-notify",
         }
     },
+    {
+        "folke/trouble.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+    },
     {"voldikss/vim-floaterm"},
     {
         "pwntester/octo.nvim",
