1bdcb7e8f3f0e4c8b806035123a8f763d2f79125
Mo Ali <akali1987@gmail.com>
2024-01-23 17:59:03 -0500

    removing autosave

--- nvim/lua/plugin-configs/null-ls.lua
+++ nvim/lua/plugin-configs/null-ls.lua
@@ -29,19 +29,19 @@ nls.setup({
     nls.builtins.formatting.terraform_fmt,
     nls.builtins.hover.dictionary,
   },
-  on_attach = function(client, bufnr)
-      if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({
-              group = augroup,
-              buffer = bufnr
-          })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-              group = augroup,
-              buffer = bufnr,
-              callback = function ()
-                vim.lsp.buf.format({ bufnr = bufnr })
-              end
-          })
-      end
-    end
+  -- on_attach = function(client, bufnr)
+  --     if client.supports_method("textDocument/formatting") then
+  --         vim.api.nvim_clear_autocmds({
+  --             group = augroup,
+  --             buffer = bufnr
+  --         })
+  --         vim.api.nvim_create_autocmd("BufWritePre", {
+  --             group = augroup,
+  --             buffer = bufnr,
+  --             callback = function ()
+  --               vim.lsp.buf.format({ bufnr = bufnr })
+  --             end
+  --         })
+  --     end
+  --   end
 })
