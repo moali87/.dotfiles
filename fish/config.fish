4e71feb49b7e6fd4deedcdd4596c63c5e0864eec
Mo Ali <akali1987@gmail.com>
2024-03-20 07:52:40 -0400

    add fzf keybindings

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,26 @@
+# fzf
+fzf --fish | source
+
+# FZF Preview file content using bat (https://github.com/sharkdp/bat)
+export FZF_CTRL_T_OPTS="
+  --walker-skip .git,node_modules,target
+  --preview 'bat -n --color=always {}'
+  --bind 'ctrl-/:change-preview-window(down|hidden|)'"
+# FZF
+# CTRL-/ to toggle small preview window to see the full command
+# CTRL-Y to copy the command into clipboard using pbcopy
+export FZF_CTRL_R_OPTS="
+  --preview 'echo {}' --preview-window up:3:hidden:wrap
+  --bind 'ctrl-/:toggle-preview'
+  --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
+  --color header:italic
+  --header 'Press CTRL-Y to copy command into clipboard'"
+# FZF
+# Print tree structure in the preview window
+export FZF_ALT_C_OPTS="
+  --walker-skip .git,node_modules,target
+  --preview 'tree -C {}'"
+
 # Aliases
 alias cat "bat --style=plain"
 
@@ -58,3 +81,4 @@ end
 
 # TFENV
 set -x PATH $HOME/.tfenv/bin $PATH
+
