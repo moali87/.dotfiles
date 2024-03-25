ffbfc542a3984367635e698b795215c91d94e599
Mo Ali <akhter.ali@upstart.com>
2024-03-25 10:50:05 -0400

    remote trailing and empty whitespace

--- nvim/lua/config/base/keymaps.lua
+++ nvim/lua/config/base/keymaps.lua
@@ -19,3 +19,8 @@ vim.api.nvim_create_autocmd("BufEnter", {
         vim.api.nvim_command("set tabstop=2")
     end
 })
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "*" },
+  command = [[%s/\s\+$//e]],
+})
