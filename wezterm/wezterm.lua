afe789aae035065ad7b8a84ab349819c87b6e00c
Mo Ali <akali1987@gmail.com>
2024-04-02 18:19:44 -0400

    Remove tokyo night and add monokai

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,9 +1,10 @@
 local wezterm = require 'wezterm'
 
 local config = {}
-config.color_scheme = 'GruvboxDark'
+-- config.color_scheme = 'GruvboxDark'
 -- config.color_scheme_dirs = {'./colorschemes'}
 -- config.color_scheme = 'Material (Gogh)'
+config.color_scheme = 'Monokai (terminal.sexy)'
 config.font = wezterm.font('Fira Code')
 config.term = 'wezterm'
 config.keys = {
