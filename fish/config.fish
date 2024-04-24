c1bce0f53f20525b12f25f310b9c8fafc97ea917
Mo Ali <akhter.ali@upstart.com>
2024-04-24 18:13:47 -0400

    remove greeting

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
+# disable fish greeting
+set fish_greeting
+
 # FZF Preview file content using bat (https://github.com/sharkdp/bat)
 export FZF_CTRL_T_OPTS="
   --walker-skip .git,node_modules,target
