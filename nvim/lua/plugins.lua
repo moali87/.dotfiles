55beea7bc2d2129cb481d286a68eb567b0682907
Mo Ali <akali1987@gmail.com>
2024-10-02 10:56:30 -0400

    Add wezterm keymaps

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -3,6 +3,21 @@ local plugins = {
         "mistricky/codesnap.nvim",
         run = "make"
     },
+    {
+        "folke/trouble.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        priority = 1000 ,
+        cmd = "Trouble",
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+        config = function ()
+            require("trouble").setup()
+            require("config.trouble.keymaps")
+        end
+    },
     { "MunifTanjim/nui.nvim" },
     {
         "MeanderingProgrammer/markdown.nvim",
@@ -31,24 +46,34 @@ local plugins = {
     { "nvim-tree/nvim-web-devicons" },
     { "chrisgrieser/cmp-nerdfont" },
     { "projekt0n/caret.nvim" },
+
+    -- Custom Parameters (with defaults)
     {
         "David-Kunz/gen.nvim",
         opts = {
-            model = "codellama", -- The default model to use.
-            display_mode = "float", -- The display mode. Can be "float" or "split".
-            show_prompt = true, -- Shows the Prompt submitted to Ollama.
-            show_model = true, -- Displays which model you are using at the beginning of your chat session.
+            model = "mistral", -- The default model to use.
+            quit_map = "q", -- set keymap for close the response window
+            retry_map = "<c-r>", -- set keymap to re-send the current prompt
+            accept_map = "<c-cr>", -- set keymap to replace the previous selection with the last result
+            host = "localhost", -- The host running the Ollama service.
+            port = "11434", -- The port on which the Ollama service is listening.
+            display_mode = "float", -- The display mode. Can be "float" or "split" or "horizontal-split".
+            show_prompt = false, -- Shows the prompt submitted to Ollama.
+            show_model = false, -- Displays which model you are using at the beginning of your chat session.
             no_auto_close = false, -- Never closes the window automatically.
+            hidden = false, -- Hide the generation window (if true, will implicitly set `prompt.replace = true`), requires Neovim >= 0.10
             init = function(options) pcall(io.popen, "ollama serve > /dev/null 2>&1 &") end,
             -- Function to initialize Ollama
-            command = "curl --silent --no-buffer -X POST http://localhost:11434/api/generate -d $body",
+            command = function(options)
+                local body = {model = options.model, stream = true}
+                return "curl --silent --no-buffer -X POST http://" .. options.host .. ":" .. options.port .. "/api/chat -d $body"
+            end,
             -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
-            -- This can also be a lua function returning a command string, with options as the input parameter.
+            -- This can also be a command string.
             -- The executed command must return a JSON object with { response, context }
             -- (context property is optional).
-            list_models = "<function>", -- Retrieves a list of model names
-            debug = true, -- Prints errors and the command which is run.
-            dev = true
+            -- list_models = '<omitted lua function>', -- Retrieves a list of model names
+            debug = false -- Prints errors and the command which is run.
         }
     },
     -- {
@@ -56,18 +81,6 @@ local plugins = {
     --     version = "^4", -- Recommended
     --     lazy = false, -- This plugin is already lazy
     -- },
-    {
-        "folke/trouble.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        },
-        config = function ()
-            require("config.trouble.keymaps")
-        end
-    },
     {"voldikss/vim-floaterm"},
     {
         "ibhagwan/fzf-lua",
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,18 +1,180 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
 
 local config = {}
+-- Show which key table is active in the status area
+wezterm.on('update-right-status', function (window, pane)
+    local name = window:active_key_table()
+    if name then
+        name = 'TABLE: ' .. name
+    end
+    window:set_right_status(name or '')
+end)
 -- config.color_scheme = 'GruvboxDark'
 -- config.color_scheme_dirs = {'./colorschemes'}
 -- config.color_scheme = 'Material (Gogh)'
 config.color_scheme = 'Monokai (terminal.sexy)'
 config.font = wezterm.font('Fira Code')
-config.term = 'wezterm'
+config.term = 'xterm-256color'
 config.keys = {
     {
         key = 'r',
         mods = 'CMD|SHIFT',
-        action = wezterm.action.ReloadConfiguration
-    }
+        action = act.ReloadConfiguration
+    },
+    {
+        key = 'y', mods = 'CTRL', action = act.ShowDebugOverlay
+    },
+    {
+        key = 'p',
+        mods = 'CMD',
+        action = act.ActivateKeyTable {
+            name = 'resize_pane',
+            timeout_milliseconds = 1000,
+        },
+    },
+    {
+        key = 'p',
+        mods = 'ALT',
+        action = act.ActivateKeyTable {
+            name = 'activate_pane',
+            timeout_milliseconds = 1000,
+        },
+    },
+    {
+        key = 'p',
+        mods = 'CTRL',
+        action = act.ActivateKeyTable {
+            name = 'create_pane',
+            timeout_milliseconds = 1000,
+        },
+    },
 }
 
+config.key_tables = {
+    -- Defines the keys that are active in our resizing-pane mode.
+    -- Since we're likely to want to make multiple adjustments,
+    -- we made the activation one_shot=false.  We therefore need
+    -- to define a key assignment for getting out of this mode.
+    -- 'resize_pane' here corresponds to the name="resize_pane" in
+    -- the key assignments above.
+    resize_pane = {
+        { key = 'LeftArrow', action = act.AdjustPaneSize { 'Left', 1 } },
+        { key = 'h', action = act.AdjustPaneSize { 'Left', 1 } },
+
+        { key = 'RightArrow', action = act.AdjustPaneSize { 'Right', 1 } },
+        { key = 'l', action = act.AdjustPaneSize { 'Right', 1 } },
+
+        { key = 'UpArrow', action = act.AdjustPaneSize { 'Up', 1 } },
+        { key = 'k', action = act.AdjustPaneSize { 'Up', 1 } },
+
+        { key = 'DownArrow', action = act.AdjustPaneSize { 'Down', 1 } },
+        { key = 'j', action = act.AdjustPaneSize { 'Down', 1 } },
+
+        -- Cancel the mode by pressing escape
+        { key = 'Escape', action = 'PopKeyTable' },
+    },
+
+    -- Defines the keys that are active in our activate-pane mode.
+    -- 'activate_pane' here corresponds to the name name="activate_pane" in
+    -- the key assignments above.
+    activate_pane = {
+        { key = 'LeftArrow', action = act.ActivatePaneDirection 'Left' },
+        { key = 'h', action =  act.ActivatePaneDirection 'Left' },
+        { key = 'f', action =  act.TogglePaneZoomState },
+
+        { key = 'RightArrow', action = act.ActivatePaneDirection 'Right' },
+        { key = 'l', action =  act.ActivatePaneDirection 'Right' },
+
+        { key = 'UpArrow', action = act.ActivatePaneDirection 'Up' },
+        { key = 'k', action =  act.ActivatePaneDirection 'Up' },
+
+        { key = 'DownArrow', action = act.ActivatePaneDirection 'Down' },
+        { key = 'j', action =  act.ActivatePaneDirection 'Down' },
+
+        -- Cancel the mode by pressing escape
+        { key = 'Escape', action = 'PopKeyTable' },
+    },
+
+    create_pane = {
+        {
+            key = 'LeftArrow',
+            action = act.SplitPane {
+                direction = 'Left',
+                size = {
+                    Percent = 50
+                }
+            }
+        },
+        {
+            key = 'h',
+            action = act.SplitPane {
+                direction = 'Left',
+                size = {
+                    Percent = 50
+                }
+            }
+        },
+        {
+            key = 'RightArrow',
+            action = act.SplitPane {
+                direction = 'Right',
+                size = {
+                    Percent = 50
+                }
+            }
+        },
+        {
+            key = 'l',
+            action = act.SplitPane {
+                direction = 'Right',
+                size = {
+                    Percent = 50
+                }
+            }
+        },
+        {
+            key = 'UpArrow',
+            action = act.SplitPane {
+                direction = 'Up',
+                size = {
+                    Percent = 50
+                }
+            }
+        },
+        {
+            key = 'k',
+            action = act.SplitPane {
+                direction = 'Up',
+                size = {
+                    Percent = 50
+                }
+            }
+        },
+        {
+            key = 'DownArrow',
+            action = act.SplitPane {
+                direction = 'Down',
+                size = {
+                    Percent = 50
+                }
+            }
+        },
+        {
+            key = 'j',
+            action = act.SplitPane {
+                direction = 'Down',
+                size = {
+                    Percent = 50
+                }
+            }
+        },
+        {
+            key = 'd',
+            action = act.CloseCurrentPane {
+                confirm = true
+            }
+        },
+    },
+}
 return config
