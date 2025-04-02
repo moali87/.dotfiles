5a9ad9e48c2f889188c10007253575ae396eddb6
Mo Ali <akhter.ali@upstart.com>
2025-04-02 12:45:33 -0400

    add lua_ls

--- nvim/lua/config/base/autocmd.lua
+++ nvim/lua/config/base/autocmd.lua
@@ -2,9 +2,9 @@ local indentGroup = vim.api.nvim_create_augroup("Indentation", { clear = true })
 local formatting = vim.api.nvim_create_augroup("Formatting", { clear = true })
 
 vim.api.nvim_create_autocmd("BufEnter", {
-    pattern = {"*.go", "*.lua", "*.ts", "*.json"},
+    pattern = { "*.go", "*.lua", "*.ts", "*.json" },
     group = indentGroup,
-    callback = function ()
+    callback = function()
         vim.api.nvim_command("set shiftwidth=4")
         vim.api.nvim_command("set softtabstop=4")
         vim.api.nvim_command("set tabstop=4")
@@ -12,16 +12,16 @@ vim.api.nvim_create_autocmd("BufEnter", {
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = {"*.go", "*.lua", "*.ts", "*.json", "*.py"},
+    pattern = { "*.go", "*.lua", "*.ts", "*.json", "*.py" },
     group = formatting,
-    callback = function ()
+    callback = function()
         vim.lsp.buf.format()
     end
 })
 
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  pattern = { "*" },
-  command = [[%s/\s\+$//e]],
+    pattern = { "*" },
+    command = [[%s/\s\+$//e]],
 })
 
 vim.api.nvim_create_autocmd("RecordingEnter", {
