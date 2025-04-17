189e3be2d3a6dffbf885a7fe004d4cf7671c3497
Mo Ali <akhter.ali@upstart.com>
2025-04-17 13:58:42 -0400

    change model

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -32,6 +32,10 @@ local plugins = {
     { "MunifTanjim/nui.nvim" },
     {
         "MeanderingProgrammer/markdown.nvim",
+        opts = {
+            file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
         name = "render-markdown",                                                      -- Only needed if you have another plugin named markdown.nvim
         dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
         -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
@@ -56,12 +60,10 @@ local plugins = {
     },
     { "nvim-tree/nvim-web-devicons" },
     { "chrisgrieser/cmp-nerdfont" },
-
-    -- Custom Parameters (with defaults)
     {
         "David-Kunz/gen.nvim",
         opts = {
-            model = "deepseek-r1",  -- The default model to use.
+            model = "qwq",          -- The default model to use.
             quit_map = "q",         -- set keymap for close the response window
             retry_map = "<c-r>",    -- set keymap to re-send the current prompt
             accept_map = "<c-cr>",  -- set keymap to replace the previous selection with the last result
@@ -85,6 +87,76 @@ local plugins = {
             -- (context property is optional).
             -- list_models = '<omitted lua function>', -- Retrieves a list of model names
             debug = false -- Prints errors and the command which is run.
+        },
+    },
+    {
+        "yetone/avante.nvim",
+        event = "VeryLazy",
+        version = false, -- Never set this value to "*"! Never!
+        opts = {
+            debug = true,
+            provider = "ollama",
+            behaviour = {
+                auto_suggestions = false,
+                ominimize_diff = false,
+                enable_cursor_planning_mode = true,
+                enable_token_counting = false
+            },
+            ollama = {
+                model = "deepseek-r1",
+                max_tokens = 8192
+            },
+            -- add any opts here
+            -- for example
+            -- openai = {
+            --     endpoint = "https://api.openai.com/v1",
+            --     model = "gpt-4o",   -- your desired model (or use gpt-4o, etc.)
+            --     timeout = 30000,    -- Timeout in milliseconds, increase this for reasoning models
+            --     temperature = 0,
+            --     max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+            --     --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+            -- },
+        },
+        -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+        build = "make",
+        -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "stevearc/dressing.nvim",
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            --- The below dependencies are optional,
+            "echasnovski/mini.pick",       -- for file_selector provider mini.pick
+            -- "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+            "hrsh7th/nvim-cmp",            -- autocompletion for avante commands and mentions
+            -- "ibhagwan/fzf-lua",            -- for file_selector provider fzf
+            "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+            -- "zbirenbaum/copilot.lua",        -- for providers='copilot'
+            {
+                -- support for image pasting
+                "HakonHarnes/img-clip.nvim",
+                event = "VeryLazy",
+                opts = {
+                    -- recommended settings
+                    default = {
+                        embed_image_as_base64 = false,
+                        prompt_for_file_name = false,
+                        drag_and_drop = {
+                            insert_mode = true,
+                        },
+                        -- required for Windows users
+                        use_absolute_path = true,
+                    },
+                },
+            },
+            -- {
+            --     -- Make sure to set this up properly if you have lazy=true
+            --     'MeanderingProgrammer/render-markdown.nvim',
+            --     opts = {
+            --         file_types = { "markdown", "Avante" },
+            --     },
+            --     ft = { "markdown", "Avante" },
+            -- },
         },
     },
     -- {
