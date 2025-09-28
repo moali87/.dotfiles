318c6256a99da9f1e0c96fa5766f3f31f1784724
Mo Ali <akali1987@gmail.com>
2025-09-28 19:02:11 -0400

    more fixes

--- nvim/init.lua
+++ nvim/init.lua
@@ -66,8 +66,15 @@ hi vertsplit guifg=fg guibg=bg
 hi Visual term=reverse cterm=reverse guibg=Grey
 set laststatus=3
 ]])
--- vim.cmd [[ autocmd RecordingEnter * set cmdheight=1 ]]
--- vim.cmd [[ autocmd RecordingLeave * set cmdheight=0 ]]
+
+vim.cmd [[ autocmd RecordingEnter * set cmdheight=1 ]]
+vim.cmd [[ autocmd RecordingLeave * set cmdheight=0 ]]
+
+-- Disable arrow keys
+imap("<up>", "<nop>")
+imap("<down>", "<nop>")
+imap("<left>", "<nop>")
+imap("<right>", "<nop>")
 
 -- Map keys
 require("config.base.keymaps")
