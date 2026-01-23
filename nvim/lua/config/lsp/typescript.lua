85c16d19208fd461335fd041f58760cf42329fed
Mo Ali <akhter.ali@upstart.com>
2026-01-23 14:26:41 -0500

    add typescript inlay hints

--- nvim/lua/config/lsp/typescript.lua
+++ nvim/lua/config/lsp/typescript.lua
@@ -12,6 +12,9 @@ return {
   cmd = { "typescript-language-server", "--stdio" },
   filetypes = { "javascript", "typescript", "typescriptreact", "javascriptreact" },
   root_markers = { 'tsconfig.json', 'package.json', 'jsconfig.json', '.git' },
+  on_attach = function(client, bufnr)
+    vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+  end,
   settings = {
     typescript = {
       inlayHints = inlayHints,
